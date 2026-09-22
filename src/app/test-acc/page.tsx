03d8f4352385bae6402a6ab474e8eb4f32bdfbb7
Xinmin Chu <78182414+xinminchu@users.noreply.github.com>
2026-09-22 14:15:04 -0500

    二分测试页4 /test-acc（复盘面板）/test-diag（诊断区）

---
+++ src/app/test-acc/page.tsx
@@ -0,0 +1,5 @@
+import TestAccInner from './TestAccInner';
+
+export default function TestAccPage() {
+  return <TestAccInner />;
+}
---
+++ src/app/test-diag/page.tsx
@@ -0,0 +1,5 @@
+import TestDiagInner from './TestDiagInner';
+
+export default function TestDiagPage() {
+  return <TestDiagInner />;
+}
